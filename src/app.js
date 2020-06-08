bccc225355be333264166c2682c1acbe3224bcbf
kpribbeck <kpribbeck@uc.cl>
2020-06-07 21:19:18 -0400

    undo last commit

--- src/app.js
+++ src/app.js
@@ -74,8 +74,7 @@ app.use(koaBody({
 }));
 
 app.use((ctx, next) => {
-  // ctx.request.method = override.call(ctx, ctx.request.body.fields || ctx.request.body);
-  ctx.request.method = override.call(ctx, ctx.request.body);
+  ctx.request.method = override.call(ctx, ctx.request.body.fields || ctx.request.body);
   return next();
 });
 
